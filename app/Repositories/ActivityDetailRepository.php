04e2cb979b872725a1cb124b08713324ef987601
赖杰 <vampirebitter@users.noreply.github.com>
2017-04-10 15:20:48 +0800

    Update ActivityDetailRepository.php

--- app/Repositories/ActivityDetailRepository.php
+++ app/Repositories/ActivityDetailRepository.php
@@ -2,13 +2,23 @@
 namespace App\Repositories;
 
 use App\Activity;
+use App\Sponsor;
+use App\Category;
 
 class ActivityDetailRepository
 {
 
   public function getCertainActivity($id)
   {
+     $activity = Activity::with('category')->where('id',$id)->first()->asArray();
+     $mainSponsor = Sponsor::select(['id','sponsor_name','sponsor_icon'])
+                            ->where('id',$activity['sponsor_id'])
+                            ->first()->asArray();
      return Activity::where('id',$id)->first();
   }
   
+  public function getMainSponsor($id)
+  {
+    
+  }
 }
