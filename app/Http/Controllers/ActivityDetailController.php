814eb58c8b9e546dda2633bb418024753e75a1d5
赖杰 <vampirebitter@users.noreply.github.com>
2017-04-10 14:52:29 +0800

    update activity page

--- app/Http/Controllers/ActivityDetailController.php
+++ app/Http/Controllers/ActivityDetailController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\ActivityDetailRepository;
 
 class ActivityDetailController extends Controller
 {
+    protected $activityDetail;
+    
+    public function __construct(ActivityDetailRepository $activityDetail)
+    {
+        $this->activityDetail = $activityDetail;
+    }
     /**
      * 页面的基本显示以及页面数据的渲染
      */
-    public function index()
+    public function index($id)
     {
-        return view('actvtDtl.index');
+        $activityDetail = $this->activityDetail->getCertainActivity($id);
+        return view('actvtDtl.index',compact('activityDetail'));
     }
 }
