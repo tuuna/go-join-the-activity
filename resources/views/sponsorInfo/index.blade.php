67648242b10e20e878338cd3be3cbad78349f729
vampirebitter <lj550566181@gmail.com>
2017-05-15 21:24:20 +0800

    update pages

--- resources/views/sponsorInfo/index.blade.php
+++ resources/views/sponsorInfo/index.blade.php
@@ -4,7 +4,73 @@
     $this->title = '主办方中心';
     ?>
     <link href="{{ asset('css/sponsor_info.css') }}" rel="stylesheet">
-<div class="banner"></div>
+    <!-- banner -->
+    <div class="container_lg bgimg_banner"></div>
+    <div class="container-default">
+        <div class="container_left">
+            <div class="sponsor_info_box">
+                <img src="{{ asset('upload/sponsorUpload/'.$sponsorInfo['sponsor_icon']) }}" alt="#" class="sponsor_logo">
+                <h3 class="sponsor_title">{{ $sponsorInfo['sponsor_name'] }}</h3>
+                <div class="sponsor_a">
+                    @if($sponsorInfo['count'])
+                        <span class="span_border"><a href="#">{{ $sponsorInfo['count'] }}</a>活动</span>
+                    @else
+                        <b class="span_border"><a href="#">{{ $sponsorInfo['count'] }}</a>活动</b>
+                    @endif
+                    <b><a href="#">{{ $sponsorInfo['follow_count'] }}</a>粉丝</b>
+                </div>
+                <div class="focus_share">
+                    <button class="btn-blue focus" id="focus">关注</button>
+                    <button class="btn-white unfocus" id="unfocus">已关注</button>
+                    <button class="btn-white share">分享</button>
+                </div>
+                <p class="sponsor_intro">{{ $sponsorInfo['description'] }}</p>
+            </div>
+            @if(is_null($sponsorInfo['weixin']))
+                <div></div>
+            @else
+                <div class="other_info">
+                    <img src="img/qrcode.jpg" alt="二维码图片">
+                    <p>多火工作室
+                        微信公众号
+                        duohuostudio
+                        扫一扫关注
+                        更多活动</p>
+                </div>
+            @endif
+        </div>
+        <div class="container_right">
+            @foreach($activityInfo as $info)
+                <div class="actvt_box">
+                    <img src="../../img/Group4.png" alt="#" href="{{ url('detail/'.$info['id']) }}">
+                    <div class="actvt_dtl">
+                        <p class="dtl_title"><a href="{{ url('detail/'.$info['id']) }}">{{ $info['title'] }}</a></p>
+                        <p class="actvt_time"><span class="icon time_icon"></span>时间：{{ $info['hold_time'] }}</p>
+                        <p class="actvt_place"><span class="icon place_icon"></span>地址：{{ $info['hold_address'] }}</p>
+                        <div class="actvt_people">
+                            <p><span class="icon people_icon"></span>报名人数：<span class="number">{{ $info['signUp_count'] }}</span></p>
+                            <p class="border_left"><span class="icon collection_icon"></span>收藏人数：{{ $info['follow_count'] }}<span class="number"></span></p>
+                        </div>
+                        <div class="check_star">
+                            <button class="btn-blue check">关注</button>
+                            <button class="btn-white star">分享</button>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            <div class="page_btn">
+                <button class="next_page"><</button>
+                <ul class="page_num">
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                </ul>
+                <button class="last_page">></button>
+            </div>
+
+        </div>
+        <div style="clear:both"></div>
+    </div>
+{{--<div class="banner"></div>
 <div class="container">
     <div class="left">
         <div class="introduce">
@@ -28,16 +94,14 @@
         @if(is_null($sponsorInfo['weixin']))
             <div></div>
         @else
-        <div class="leftdown">
-            <embed class="ma" src="img/ma.svg" type="image/svg+xml" />
-            <ul>
-                <li>多火工作室</li>
-                <li>微信公众号</li>
-                <li>duohuostudio</li>
-                <li>扫一扫关注</li>
-                <li>更多活动</li>
-            </ul>
-        </div>
+            <div class="other_info">
+                <img src="img/qrcode.jpg" alt="二维码图片">
+                <p>多火工作室
+                    微信公众号
+                    duohuostudio
+                    扫一扫关注
+                    更多活动</p>
+            </div>
         @endif
     </div>
     <div class="right">
@@ -69,5 +133,5 @@
             <button class="but-hou buttongray">></button>
         </div>
     </div>
-</div>
+</div>--}}
 @endsection
